57b71534e5d7e7210fb1e89378b6247e5ba000e6
Jeanpy171 <jeanpyfuentes67@gmail.com>
2025-09-20 13:13:39 -0500

    integrate IAC, api gateway, lambdas - integrate mechanica lambda

--- src/domains/sales/lambdas/add_sale/main.py
+++ src/domains/sales/lambdas/add_sale/main.py
@@ -6,11 +6,6 @@ from repositories.sale_detail_repository import SaleDetailRepository
 from repositories.sale_repository import SaleRepository
 from use_cases.sale_use_case import SaleUseCase
 
-# from repositories.customer_repository import CustomerRepository
-# from repositories.seller_repository import SellerRepository
-# from use_cases.customer_use_case import CustomerUseCase
-# from use_cases.seller_use_case import SellerUseCase
-
 db_client = DBClient.get_client()
 sale_repository = SaleRepository(db_client)
 sale_detail_repository = SaleDetailRepository(db_client)
@@ -20,23 +15,6 @@ use_case = SaleUseCase(sale_repository, sale_detail_repository)
 def lambda_handler(event, context):
     try:
         sale_data = load_initial_parameters(event)
-
-        # customer_repository = CustomerRepository(db_client)
-        # customer_use_case = CustomerUseCase(customer_repository)
-        # is_customer_exists = customer_use_case.is_customer_exists(sale_data["sale"]["id_customer"])
-        # if not is_customer_exists:
-        #     return {
-        #         "statusCode": 400,
-        #         "body": json.dumps({"message": f"El cliente con id {sale_data["sale"]["id_customer"]} no existe"})
-        #     }
-        # seller_repository = SellerRepository(db_client)
-        # seller_use_case = SellerUseCase(seller_repository)
-        # is_seller_exists = seller_use_case.is_seller_exists(sale_data["sale"]["id_seller"])
-        # if not is_seller_exists:
-        #     return {
-        #         "statusCode": 400,
-        #         "body": json.dumps({"message": f"El vendedor con id {sale_data["sale"]["id_seller"]} no existe"})
-        #     }
         response_sale = use_case.add_sale(sale_data)
 
         return {
