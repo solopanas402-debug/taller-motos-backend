2a483fcd130eb6b3555d7f6e98bc5ce70513a0bf
LeoClearMinds <leonardorh1718@gmail.com>
2025-09-28 18:22:21 -0500

    Revert "update entity for add discount and model"
    This reverts commit cc20545573d53288c88332ebb119a4f103bc4687.

--- layers/shared/entities/product.py
+++ layers/shared/entities/product.py
@@ -7,7 +7,7 @@ class Product:
     def __init__(self, id_product: str, code: str, name: str, description: str, price: float, discount: float,
                  stock: int,
                  min_stock: int, max_stock: int,
-                 id_supplier: str, id_category: str, id_brand: str, model: str, active: bool, created_at: datetime,
+                 id_supplier: str, id_category: str, id_brand: str, active: bool, created_at: datetime,
                  updated_at: datetime):
         self.id_product = id_product
         self.code = code
@@ -21,7 +21,6 @@ class Product:
         self.id_supplier = id_supplier
         self.id_category = id_category
         self.id_brand = id_brand
-        self.model = model
         self.active = active
         self.created_at = created_at
         self.updated_at = updated_at
@@ -30,10 +29,10 @@ class Product:
     def from_dict(cls, row: dict):
         return cls(id_product=row.get('id_product', generate_uuid_str()), code=row.get('code'), name=row.get('name'),
                    description=row.get('description'),
-                   price=row.get('price'), discount=row.get('discount'), stock=row.get('stock'),
+                   price=row.get('price'), discount=row.get('discount', 0.00), stock=row.get('stock'),
                    min_stock=row.get('min_stock'), max_stock=row.get('max_stock'), id_supplier=row.get('id_supplier'),
                    id_category=row.get('id_category'),
-                   id_brand=row.get('id_brand'), model=row.get('model'), active=row.get('active'),
+                   id_brand=row.get('id_brand'), active=row.get('active'),
                    created_at=row.get('created_at', datetime.now(timezone.utc)), updated_at=row.get('updated_at'))
 
     def to_dict(self):
@@ -43,14 +42,12 @@ class Product:
             "name": self.name,
             "description": self.description,
             "price": self.price,
-            "discount": float(self.discount) if self.discount is not None else 0.0,
             "stock": self.stock,
             "min_stock": self.min_stock,
             "max_stock": self.max_stock,
             "id_supplier": self.id_supplier,
             "id_category": self.id_category,
             "id_brand": self.id_brand,
-            "model": self.model,
             "active": self.active,
             "created_at": self.created_at.isoformat() if self.created_at else None,
             "updated_at": self.updated_at.isoformat() if self.updated_at else None
