7048e161cacc5ab1663d93004214b1e4f74b56fe
Jeanpy171 <jeanpyfuentes67@gmail.com>
2025-10-01 22:09:42 -0500

    update template.yml for CRUD operations

---
+++ src/domains/sales/lambdas/delete_sale/main.py
@@ -0,0 +1,11 @@
+import json
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    print("Context: " + json.dumps(context, indent=2))
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
